13c3400b51de5d3512aa611811c6ee198a0e989f
Vivider <118007939+Vivider10@users.noreply.github.com>
2026-09-01 20:40:22 -0400

    Load kitchen board styles

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 import GreasyPOS from '../components/GreasyPOS'
+import '../kitchen.css'
 
 export const Route = createFileRoute('/')({
   component: Home,
